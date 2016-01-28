fd096ca5332612371b02f70cb1c487038ef5eee9
Sorasack Ngam-Kaew <Sorasack.Lempp@gmx.de>
2016-01-28 21:02:40 +0100

    vx

---
+++ biblio-service/src/test/java/de/hrw/swep/biblio/service/BibliothekTestMitMock.java
@@ -0,0 +1,46 @@
+package de.hrw.swep.biblio.service;
+
+
+
+import org.junit.Test;
+
+/**
+ * Testet die Bibliotheks-Klasse mit einem Mock-Objekt
+ * @author Sandra
+ *
+ */
+public class BibliothekTestMitMock {
+
+	/**
+	 * Testet, ob ein Buch mit gegebenem Titel gefunden wird.
+	 */
+	@Test
+	public void testSucheBuchNachTitel() {
+		
+	}
+
+	/**
+	 * Testet, ob ein Buch mit gegebenem Autor gefunden wird.
+	 */
+	@Test
+	public void testSucheBuchNachAutor() {
+		
+	}
+
+	/**
+	 * Testet, ob ein Benutzer mit gegebenem Namen gefunden wird.
+	 */
+	@Test
+	public void testSucheBenutzerNachName() {
+		
+	}
+
+	/**
+	 * Testet, ob ein Benutzer mit einer gegebenen ID gefunden wird.
+	 */
+	@Test
+	public void testSucheBenutzerNachId() {
+		
+	}
+
+}
